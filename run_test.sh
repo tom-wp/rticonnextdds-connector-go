6c0f94f2d3c51e74941d418b81f2919993ff5a72
Thomas Wynne-Powell <twp@arenko.group>
2020-06-17 09:23:50 +0100

    Fix Darwin test script

--- run_test.sh
+++ run_test.sh
@@ -4,8 +4,7 @@ go get -v github.com/stretchr/testify
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	export LD_LIBRARY_PATH=$PWD/rticonnextdds-connector/lib/x64Linux2.6gcc4.4.5:$LD_LIBRARY_PATH
-	go test -v -race -coverprofile=coverage.txt -covermode=atomic 
+	go test -v -race -coverprofile=coverage.txt -covermode=atomic
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	export DYLD_LIBRARY_PATH=rticonnextdds-connector/lib/x64Darwin16clang8.0:$DYLD_LIBRARY_PATH
-	go test -v -race -coverprofile=coverage.txt -covermode=atomic 
+	go test -exec "env DYLD_LIBRARY_PATH=$(dirname "$(greadlink -f "${BASH_SOURCE[0]}")")/rticonnextdds-connector/lib/x64Darwin16clang8.0" -v -race -coverprofile=coverage.txt -covermode=atomic
 fi
